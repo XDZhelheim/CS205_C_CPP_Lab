5109a46bc3f47fecad571f5667e3e26aca2472c3
XDZhelheim <601251915@qq.com>
2021-09-24 21:52:53 +0800

    matrix add, sub, slice

--- project2/matmul.cpp
+++ project2/matmul.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include <ctime>
 
-#define DEBUG 0
+#define DEBUG 1
 
 #define USE_DOUBLE 1
 
@@ -71,7 +71,7 @@ matrix multiply_matrix(matrix m1, matrix m2) {
     int intermediate = m2.size();
 
     if (intermediate != m1[0].size()) {
-        cout << "Wrong matrix dimension." << endl;
+        cout << "Multiplication error: matrix dimension cannot match." << endl;
         exit(EXIT_FAILURE);
     }
 
@@ -86,7 +86,136 @@ matrix multiply_matrix(matrix m1, matrix m2) {
     return product;
 }
 
+matrix add_matrix(matrix m1, matrix m2) {
+    if (m1.size()!=m2.size() || m1[0].size()!=m2[0].size()) {
+        cout<<"Add error: matrix dimension cannot match."<<endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    matrix res(m1.size(), vector<REAL_NUMBER>(m1[0].size(), 0));
+
+    for (int i=0;i<m1.size();i++)
+        for (int j=0;j<m1[0].size();j++) {
+            res[i][j]=m1[i][j]+m2[i][j];
+        }
+
+    return res;
+}
+
+matrix sub_matrix(matrix m1, matrix m2) {
+    if (m1.size()!=m2.size() || m1[0].size()!=m2[0].size()) {
+        cout<<"Subtract error: matrix dimension cannot match."<<endl;
+        exit(EXIT_FAILURE);
+    }
+
+    matrix res(m1.size(), vector<REAL_NUMBER>(m1[0].size(), 0));
+
+    for (int i=0;i<m1.size();i++)
+        for (int j=0;j<m1[0].size();j++) {
+            res[i][j]=m1[i][j]-m2[i][j];
+        }
+
+    return res;
+}
+
+// [start, end)
+matrix slice_matrix(matrix m, int row_start, int row_end, int col_start, int col_end) {
+    matrix res;
+
+    for (int i=row_start;i<row_end;i++) {
+        res.push_back(vector<REAL_NUMBER>(m[i].begin()+col_start, m[i].begin()+col_end));
+    }
+
+    return res;
+}
+
+matrix merge_matrix(matrix C11, matrix C12, matrix C21, matrix C22) {
+
+}
+
+void free_matrix(matrix &m) {
+    // m.clear();
+    // m.shrink_to_fit();
+    matrix().swap(m);
+}
+
+matrix strassen(matrix A, matrix B) {
+    if (A.size()!=B.size() || A[0].size()!=B[0].size()) {
+        cout<<"Strassen multiplication error: matrix dimension cannot match."<<endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int N=A.size();
+
+    if (N & (N-1)!=0) {
+        cout<<"Strassen multiplication error: matrix dimension is not 2^n."<<endl;
+        exit(EXIT_FAILURE);
+    }
+
+    matrix A11=slice_matrix(A, 0, N/2, 0, N/2);
+    matrix A12=slice_matrix(A, 0, N/2, N/2, N);
+    matrix A21=slice_matrix(A, N/2, N, 0, N/2);
+    matrix A22=slice_matrix(A, N/2, N, N/2, N);
+
+    matrix B11=slice_matrix(B, 0, N/2, 0, N/2);
+    matrix B12=slice_matrix(B, 0, N/2, N/2, N);
+    matrix B21=slice_matrix(B, N/2, N, 0, N/2);
+    matrix B22=slice_matrix(B, N/2, N, N/2, N);
+
+    matrix S1=sub_matrix(B12, B22);
+    matrix S2=add_matrix(A11, A12);
+    matrix S3=add_matrix(A21, A22);
+    matrix S4=sub_matrix(B21, B11);
+    matrix S5=add_matrix(A11, A12);
+    matrix S6=add_matrix(B11, B22);
+    matrix S7=sub_matrix(A12, A22);
+    matrix S8=add_matrix(B21, B22);
+    matrix S9=sub_matrix(A11, A21);
+    matrix S10=add_matrix(B11, B12);
+
+    matrix P1=strassen(A11, S1);
+    matrix P2=strassen(S2, B22);
+    matrix P3=strassen(S3, B11);
+    matrix P4=strassen(A22, S4);
+    matrix P5=strassen(S5, S6);
+    matrix P6=strassen(S7, S8);
+    matrix P7=strassen(S9, S10);
+
+    matrix C11=add_matrix(P5, P4);
+    C11=sub_matrix(C11, P2);
+    C11=add_matrix(C11, P6);
+    matrix C12=add_matrix(P1, P2);
+    matrix C21=add_matrix(P3, P4);
+    matrix C22=add_matrix(P5, P1);
+    C22=sub_matrix(C22, P3);
+    C22=sub_matrix(C22, P7);
+
+    matrix C=merge_matrix(C11, C12, C21, C22);
+
+    free_matrix(S1);
+    // TODO: free
+
+    return C;
+}
+
+void debug_test() {
+    matrix m1 = read_matrix("H:\\Codes\\C-CppWorkspace\\CS205_C_CPP_Lab\\project2\\mat-A-32.txt");
+    matrix m2 = read_matrix("H:\\Codes\\C-CppWorkspace\\CS205_C_CPP_Lab\\project2\\mat-B-32.txt");
+
+    cout << "capacity: "<<m1.capacity() << endl;
+	cout << "size: " << m1.size() << endl;
+    free_matrix(m1);
+    cout << "capacity: "<<m1.capacity() << endl;
+	cout << "size: " << m1.size() << endl;
+
+    exit(EXIT_SUCCESS);
+}
+
 int main(int argc, char const *argv[]) {
+    if (DEBUG) {
+        debug_test();
+    }
+
     if (argc != 4) {
         cout << "Wrong number of arguments.";
         exit(EXIT_FAILURE);
